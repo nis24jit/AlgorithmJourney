651686681e68f3c7dac9b88d419ef0b44d50fb09
Nishanth Suresh <Sudhina@Sudhies-MacBook-Air.local>
2018-02-03 18:00:29 +0100

    InsertionSort.

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,20 +1,19 @@
 function InsertionSort(arr) {
-
     var length = arr.length;
     var j;
-    for (var i = 1; i < length; i++) {
-        var temp = arr[i];
-        j = i;
-        while (j > 0 && arr[j - 1] > temp) {
-            arr[j] = arr[j-1];
-            j--;
+    for (var i = 0; i < length; i++) {
+        for (var j = i; j > 0; j--)
+
+        {
+            if (arr[j] < arr[j - 1]) {
+                var temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            }
         }
-        arr[j] = temp;
+
     }
-
-
     console.log(arr);
 }
 
-InsertionSort([8, 5, 1, 4]);
-
+InsertionSort([2,1,4,3])
